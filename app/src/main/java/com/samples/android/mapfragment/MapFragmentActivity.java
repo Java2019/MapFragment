5ee5a256a0e38679fd77eb8efd5f0452b54408e1
java2019 <java_2019@mail.ru>
2017-07-02 12:52:34 +0300

    final

--- app/src/main/java/com/samples/android/mapfragment/MapFragmentActivity.java
+++ app/src/main/java/com/samples/android/mapfragment/MapFragmentActivity.java
@@ -3,19 +3,29 @@ package com.samples.android.mapfragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
-public class MapFragmentActivity extends AppCompatActivity {
+public class MapFragmentActivity extends AppCompatActivity
+        implements OnMapReadyCallback{
 
     static final LatLng HAMBURG = new LatLng(53.558, 9.927);
     static final LatLng KIEL = new LatLng(53.551, 9.993);
-    private GoogleMap map;
+    private SupportMapFragment map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_my);
-        //map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        setContentView(R.layout.activity_map_fragment);
+        map = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        map.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
     }
 }
